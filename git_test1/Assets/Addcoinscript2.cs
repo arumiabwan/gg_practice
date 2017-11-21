880adc9e5a83ef4fb18b71e7aa8e19286a53a076
aluminum88 <hogehoge@gmail.com>
2017-11-21 10:43:39 +0900

    押したらひとつ出せるAddcoinscript2を追加

---
+++ git_test1/Assets/Addcoinscript2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addcoinscript2 : MonoBehaviour
+{
+
+    public Transform Basecoin2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.X))
+        {
+
+            Vector3 offset = new Vector3(0, 0, 1);
+
+            Instantiate(Basecoin2, transform.position, transform.rotation);
+
+        }
+
+    }
+}
